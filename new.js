b8f32222e9ac1ab6486c9b1a8f452ca0c93cffa8
Niaz est <estefaie.niaz@gmail.com>
2022-11-15 14:51:31 +0330

    refactor: :fire: some files removed

--- new.js
+++
@@ -1,7 +0,0 @@
-findLongWords = (text) => {
-  const arrayedWords = text.toLowerCase().split(" ");
-  const trimmedWords = arrayedWords.map((word) => ());
-  console.log(trimmedWords);
-
-  // return result.slice(0, -2);
-};
